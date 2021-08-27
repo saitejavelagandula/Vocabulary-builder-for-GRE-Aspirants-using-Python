1c404178906c572adb9e2d13d1202bff374cf830
saitejavelagandula <saitejavelagandula@gmail.com>
2021-08-27 14:38:48 +0530

    removed redundant comments

--- main.py
+++ main.py
@@ -1,56 +1,69 @@
-#python module to get the copied word.
+#import python module to get the copied word.
 import pyperclip as pc
 
-#keyboard module to capture the hotkey
+#import python keyboard module to capture the hotkey
 import keyboard
 
-#wordnet module is used as dictionary
+#import python wordnet package is used as dictionary
 from nltk.corpus import wordnet
 
-import tkinter as tk
-
+#import python GUI package 
 from tkinter import *
 
+#import python automation package
 import pyautogui as pya
 
 #function to get meaning of a copied word
 def getMeaning():
 
-	#wait till user triggers ctrl+shift+a
-
+		#wait till user triggers ctrl+shift+a
 		keyboard.wait('ctrl + shift + a')
 
+		#execute ctrl+c on selected text
 		pya.hotkey('ctrl', 'c')
 
+		#assign copied word to a variable
 		word = pc.paste()
-		#get synsets of the copied word using wordnet
-		word_synset = wordnet.synsets(word)
+
+		#get list of synsets of the copied word using wordnet
+		word_synsets = wordnet.synsets(word)
+
+		#create a list
 		meanings = []
-		for synset in word_synset:
+
+		#iterate through synsets list, get meaning of each synset and append meaning to the meanings list
+		for synset in word_synsets:
 			meanings.append(synset.definition())
 
-		# convert_list_to_string = [str(element) for element in meanings]
+		#creates a window
+		window = Tk()
 
-		# meanings_joined_string = ",\n".join(convert_list_to_string)
-
-		window = tk.Tk()
-		scrollbar = Scrollbar(window)
+		#get width and height of user screen
 		width= window.winfo_screenwidth()               
 		height= window.winfo_screenheight()
-		window.title("Vocabulary Builder for GRE aspirants")
+
+		#creates window based on user's screen dimensions
 		window.geometry("%dx%d" % (width, height))
+
+		#title of the window
+		window.title("Vocabulary Builder for GRE aspirants")
+
+		#creates a scrollbar widget for the window
+		scrollbar = Scrollbar(window)
+
+		#creates list widget
 		mylist = Listbox(window, yscrollcommand = scrollbar.set)
+
+		#insert meaning from meanings list into list widget 
 		for i in meanings:
 			mylist.insert(END, i)
+
 		mylist.pack( side = LEFT, fill = BOTH, expand = 1)
+
 		scrollbar.config( command = mylist.yview )			
-		# name = tk.Label(window, text = )
-		# name.pack()
+
 		window.mainloop()
-		#output the meaning of a word to console
-		print(meaning[0].definition())
 
 		getMeaning()
 				
-
-getMeaning()	
+getMeaning()
