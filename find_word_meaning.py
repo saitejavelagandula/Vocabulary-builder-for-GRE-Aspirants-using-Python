0176cf714e17c0c1df51c9a9b76809fc38f6f7d0
saitejavelagandula <saitejavelagandula@gmail.com>
2021-08-27 12:35:07 +0530

    finished finding meaning of a word using a hotkey

--- find_word_meaning.py
+++
@@ -1,7 +0,0 @@
-from nltk.corpus import wordnet
-class Find_word_meaning:
-	
-	def word_meaning(word):
-		print("here")
-		meaning = wordnet.synsets(word)
-		return meaning[0].definition()
--- main.py
+++ main.py
@@ -1,17 +1,56 @@
-#module to get the copied word.
+#python module to get the copied word.
 import pyperclip as pc
-#keyboard module
+
+#keyboard module to capture the hotkey
 import keyboard
+
+#wordnet module is used as dictionary
 from nltk.corpus import wordnet
-from find_word_meaning import Find_word_meaning
 
-def check():
-	keyboard.wait('ctrl + shift + a')
+import tkinter as tk
 
-	find_word_meaning_class_obj = Find_word_meaning
+from tkinter import *
 
-	print(find_word_meaning_class_obj.word_meaning(pc.paste()))
+import pyautogui as pya
 
-	check()
+#function to get meaning of a copied word
+def getMeaning():
 
-check()	
+	#wait till user triggers ctrl+shift+a
+
+		keyboard.wait('ctrl + shift + a')
+
+		pya.hotkey('ctrl', 'c')
+
+		word = pc.paste()
+		#get synsets of the copied word using wordnet
+		word_synset = wordnet.synsets(word)
+		meanings = []
+		for synset in word_synset:
+			meanings.append(synset.definition())
+
+		# convert_list_to_string = [str(element) for element in meanings]
+
+		# meanings_joined_string = ",\n".join(convert_list_to_string)
+
+		window = tk.Tk()
+		scrollbar = Scrollbar(window)
+		width= window.winfo_screenwidth()               
+		height= window.winfo_screenheight()
+		window.title("Vocabulary Builder for GRE aspirants")
+		window.geometry("%dx%d" % (width, height))
+		mylist = Listbox(window, yscrollcommand = scrollbar.set)
+		for i in meanings:
+			mylist.insert(END, i)
+		mylist.pack( side = LEFT, fill = BOTH, expand = 1)
+		scrollbar.config( command = mylist.yview )			
+		# name = tk.Label(window, text = )
+		# name.pack()
+		window.mainloop()
+		#output the meaning of a word to console
+		print(meaning[0].definition())
+
+		getMeaning()
+				
+
+getMeaning()	
